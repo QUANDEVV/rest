babb1af26c41a3463d1b2732f56f5723b7f41382
QUANDEVV <vercelploy@gmail.com>
2023-10-15 16:51:49 +0300

    Done

--- rest/settings.py
+++ rest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-67()*kg*%sn(s%rk!&=$pry4bixxjg1dm8r)rcj-ikp-h$3f@h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
