b2767b3ebc49dd19a57ad4d7b750afa600143e4f
QUANDEVV <vercelploy@gmail.com>
2023-10-17 10:06:49 +0300

    Done

--- rest/settings.py
+++ rest/settings.py
@@ -155,10 +155,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-
-
-
-
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -168,4 +164,7 @@ AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE')
 AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL')
 AWS_S3_VERIFY = config('AWS_S3_VERIFY', default=True, cast=bool)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
 
